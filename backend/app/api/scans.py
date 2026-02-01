a610e7382f7294050f45da82f42e0d3afb28ca80
Andrew Nieves <tun29995@temple.edu>
2026-01-31 23:36:11 -0500

    Scan API Endpoint (scans.py)

---
+++ backend/app/api/scans.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from app.scanners.xss import scan_xss
+
+router = APIRouter(prefix="/scans")
+
+@router.post("/")
+def start_scan(target_url): str):
+    results = scan_xss(target_url)
+    return {
+        "results": results
+    }
