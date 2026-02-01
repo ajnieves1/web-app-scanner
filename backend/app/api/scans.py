3f880198ca2915ce59818dfde121c0fb2e14ec3e
Andrew Nieves <tun29995@temple.edu>
2026-02-01 00:45:06 -0500

    Created scans.py

--- backend/app/api/scans.py
+++ backend/app/api/scans.py
@@ -4,7 +4,7 @@ from app.scanners.xss import scan_xss
 router = APIRouter(prefix="/scans")
 
 @router.post("/")
-def start_scan(target_url): str):
+def start_scan(target_url: str):
     results = scan_xss(target_url)
     return {
         "results": results
